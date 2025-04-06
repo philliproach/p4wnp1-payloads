cdcd7ef8e6ef4681e6227a3dbaf5e2fd0f335ca9
Phillip Roach <108208254+philliproach@users.noreply.github.com>
2025-04-06 00:01:14 -0400

    Update rick-roll-windows.js

--- payloads/fun-scripts/rick-roll/rick-roll-windows.js
+++ payloads/fun-scripts/rick-roll/rick-roll-windows.js
@@ -1,5 +1,11 @@
-layout('us');
+//Opens Powershell, turns volume all the way up, closes Powershell and opens rick roll in the default browser
+
+layout('US');
+typingSpeed(5,5);
 press("GUI r");
-delay(500);
-type("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-press("ENTER");
+delay(100);
+type("powershell\n");
+delay(3000);
+type('$vol = (New-Object -ComObject WScript.Shell)\n');
+type('for ($i=0; $i -lt 50; $i++) { $vol.SendKeys([char]175) }\n');
+type('Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; exit\n');
